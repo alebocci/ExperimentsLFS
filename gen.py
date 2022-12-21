a9e2e7e2aa7c457c252e6275027bcf6ac7ce48d8
alebocci <alessandro.bocci@phd.unipi.it>
2022-12-21 09:36:25 +0100

    added generators

--- gen.py
+++ gen.py
@@ -36,7 +36,11 @@ def findConf(tot):
     c = math.ceil(tot/22)
     f = math.ceil(tot/22*7)
     e = tot - c - f
-    return (c,f,e, tot)
+
+    gmin = c - 1
+    gmax = gmin * 3
+    
+    return (c,f,e,gmin,gmax,tot)
 
 while(tot<=220):
     config.append(findConf(tot))
@@ -45,10 +49,11 @@ while(tot<=220):
 for c in config:
     with open(tempConf,'w') as t:
         t.write('number_of_nodes:\n  cloud: '+str(c[0])+'\n  fog: '+str(c[1])+'\n  edge: '+str(c[2])+'\n\n')
+        t.write('event_generators:\n  generator_base_name : device\n  generators:\n    min_quantity: '+str(c[3])+'\n    max_quantity: '+str(c[4])+'\n  events_per_generator:\n    min_quantity: 4\n    max_quantity: 4\n  on_edge_probability: 0.75\n  events:\n    - event1\n    - event2\n    - event3\n    - event4\n    - event5\n    - event6\n    - event7\n\n')
 
     os.chdir('input')
     os.chdir('points')
-    with open(str(c)+'.yaml','wb') as dest:
+    with open(str(c[5])+'.yaml','wb') as dest:
         os.chdir('..')
         os.chdir('..')
         with open(tempConf,'rb') as head:
